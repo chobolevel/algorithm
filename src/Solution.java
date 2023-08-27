d716b1cb594a5f5d83d7490d1c00c9f7e77ed755
chobolevel <rodaka123@naver.com>
2023-08-27 23:48:49 +0900

    문제) 카운트 업

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,17 @@
+import java.util.stream.IntStream;
+
 public class Solution {
 
-  public int solution(int a, int b, boolean flag) {
-    return flag ? a + b : a - b;
+  public static int[] solution(int start_num, int end_num) {
+    int[] array = IntStream.rangeClosed(start_num, end_num).toArray();
+    return array;
+  }
+
+  public static void main(String[] args) {
+    int[] solution = solution(3, 10);
+    for(int i : solution) {
+      System.out.println(i);
+    }
   }
 
 }
