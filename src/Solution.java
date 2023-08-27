cba26c30efca1c7377244e0c66016e2655f840ca
chobolevel <rodaka123@naver.com>
2023-08-28 00:59:48 +0900

    문제) 홀짝에 따라 다른 값 반환하기

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,18 @@
+import java.util.stream.IntStream;
+
 public class Solution {
 
-  public static int solution(int number, int n, int m) {
-    return number % n == 0 && number % m == 0 ? 1 : 0;
+  public static int solution(int n) {
+    boolean isEven = n % 2 == 0;
+    if(isEven) {
+      return IntStream.rangeClosed(1, n).filter(i -> i % 2 == 0).map(i -> i * i).sum();
+    } else {
+      return IntStream.rangeClosed(1, n).filter(i -> i % 2 != 0).sum();
+    }
   }
 
   public static void main(String[] args) {
-    System.out.println(solution(55, 10, 5));
+    System.out.println(solution(10));
   }
 
 }
