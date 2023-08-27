6462452b9e6d96b0ffcefba2de73b270d63aed8f
chobolevel <rodaka123@naver.com>
2023-08-28 01:10:39 +0900

    문제) 조건 문자열(레전드)

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,17 @@
+import java.util.Map;
+import java.util.function.BiFunction;
+
 public class Solution {
 
   public static int solution(String ineq, String eq, int n, int m) {
-    if(ineq.equals(">")) {
-      if(eq.equals("=")) {
-        return n >= m ? 1 : 0;
-      } else {
-        return n > m ? 1 : 0;
-      }
-    } else {
-      if(eq.equals("=")) {
-        return n <= m ? 1 : 0;
-      } else {
-        return n < m ? 1 : 0;
-      }
-    }
+    Map<String, BiFunction<Integer, Integer, Boolean>> functions = Map.of(
+      ">=", (a, b) -> a >= b,
+      "<=", (a, b) -> a <= b,
+      ">!", (a, b) -> a > b,
+      "<!", (a, b) -> a < b
+    );
+
+    return functions.get(ineq + eq).apply(n, m) ? 1 : 0;
   }
 
   public static void main(String[] args) {
