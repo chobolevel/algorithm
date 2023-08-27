0edf8374b7c82d2549db22009358bfca3659619c
chobolevel <rodaka123@naver.com>
2023-08-28 00:15:07 +0900

    문제) 문자열 섞기

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,16 @@
-import java.util.Arrays;
-
 public class Solution {
 
-  public static int[] solution(int[] arr) {
-    return Arrays.stream(arr).map((i) -> {
-      int n = i;
-      if(i >= 50 && i % 2 == 0) {
-        n = i / 2;
-      } else if(i < 50 && i % 2 != 0) {
-        n = i * 2;
-      }
-      System.out.println(n);
-      return n;
-    }).toArray();
+  public static String solution(String str1, String str2) {
+    String result = "";
+    for(int i = 0; i < str1.length(); i++) {
+      result += str1.charAt(i);
+      result += str2.charAt(i);
+    }
+    return result;
   }
 
   public static void main(String[] args) {
-    int[] arr = {1, 2, 3, 100, 99, 98};
-    solution(arr);
-//    for(int i : arr) {
-//      System.out.println(i);
-//    }
+    System.out.println(solution("aaaaa", "bbbbb"));
   }
 
 }
