358f5a6a60cfe518ce1bdadad3251571249ac7d0
chobolevel <rodaka123@naver.com>
2023-08-27 23:39:21 +0900

    해답) n번째 원소부터

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Solution {
 
-  public static int[] solution(int[] num_list, int n) {
-    List<Integer> resultList = new ArrayList<>();
-    for(int i = 1; i <= num_list.length; i++) {
-      if(i >= n) {
-        resultList.add(num_list[i - 1]);
-      }
-    }
-    int[] result = new int[resultList.size()];
-    for(int i = 0; i < resultList.size(); i++) {
-      result[i] = resultList.get(i);
-    }
-    return result;
+  public int[] solution(int[] num_list, int n) {
+    return Arrays.copyOfRange(num_list, n - 1, num_list.length);
   }
 
-  public static void main(String[] args) {
-    int[] num_list = {5, 2, 1, 7, 5};
-    int n = 2;
-    int[] solution = solution(num_list, n);
-    for(int i : solution) {
-      System.out.println(i);
-    }
-  }
 }
