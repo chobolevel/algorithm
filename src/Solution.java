26fddd2d13af7ea7d01471bf50c09b0adc3a2040
chobolevel <rodaka123@naver.com>
2023-08-28 00:24:23 +0900

    문제) 더 크게 합치기

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,13 @@
 public class Solution {
 
-  public static String solution(String my_string, int k) {
-    return my_string.repeat(k);
+  public static int solution(int a, int b) {
+    String s1 = String.valueOf(a);
+    String s2 = String.valueOf(b);
+    return Integer.parseInt(s1 + s2) >= Integer.parseInt(s2 + s1) ? Integer.parseInt(s1 + s2) : Integer.parseInt(s2 + s1);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solution(89, 8));
   }
 
 }
