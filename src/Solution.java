d9fa007b8fe4150a2215d02cf415f4e8c4cf88d9
chobolevel <rodaka123@naver.com>
2023-08-29 01:44:24 +0900

    문제) 문자열 바꿔서 찾기

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,16 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Solution {
 
-  public static String solution(String myString) {
-    String result = "";
-    for(String s : myString.split("")) {
-      if(s.equals(s.toLowerCase())) {
-        result += s;
-      } else {
-        result += s.toLowerCase();
-      }
-    }
-    return result;
+  public static int solution(String myString, String pat) {
+    String str = Stream.of(myString.split("")).map(s -> s.equals("A") ? "B" : "A").collect(Collectors.joining());
+    return str.contains(pat) ? 1 : 0;
   }
 
   public static void main(String[] args) {
     int[] arr = {5, 7, 8, 3};
-    System.out.println(solution("aBcDeFg"));
+    System.out.println(solution("ABBAA", "AABB"));
   }
 
 }
