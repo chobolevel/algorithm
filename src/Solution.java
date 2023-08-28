a13f4bbcdd9bc69e58e6b8c39c26477d82f44d73
chobolevel <rodaka123@naver.com>
2023-08-29 00:52:57 +0900

    문제) rny_string

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,20 @@
 public class Solution {
 
-  public static int solution(int[] numbers, int n) {
-    int result = 0;
-    for(int i : numbers) {
-      if(result > n) {
-        break;
+  public static String solution(String rny_string) {
+    String result = "";
+    for(String s : rny_string.split("")) {
+      if(s.equals("m")) {
+        result += "rn";
       } else {
-        result += i;
+        result += s;
       }
     }
     return result;
-    // return IntStream.of(numbers).reduce((acc, cur) -> acc > n ? acc : acc + cur);
   }
 
   public static void main(String[] args) {
     int[] arr = {58, 44, 27, 10, 100};
-    System.out.println(solution(arr, 139));
+    System.out.println(solution("masterpiece"));
   }
 
 }
