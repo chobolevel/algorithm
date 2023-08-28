0eed5bf810012143e731dc253eff388656bc2a00
chobolevel <rodaka123@naver.com>
2023-08-29 00:18:38 +0900

    문제) 문자열의 뒤의 n글자

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,12 @@
-import java.util.stream.Stream;
-
 public class Solution {
 
-  public static int solution(String my_string, String is_prefix) {
-    String[] prefixArr = new String[my_string.length()];
-    for(int i = 0; i < my_string.length(); i++) {
-      prefixArr[i] = my_string.substring(0, i);
-    }
-    long count = Stream.of(prefixArr).filter((prefix) -> prefix.equals(is_prefix)).count();
-    return count > 0 ? 1 : 0;
+  public static String solution(String my_string, int n) {
+    int begin = my_string.length() - n;
+    return my_string.substring(begin);
   }
 
   public static void main(String[] args) {
-    System.out.println(solution("banana", "ban"));
+    System.out.println(solution("ProgrammerS123", 11));
   }
 
 }
