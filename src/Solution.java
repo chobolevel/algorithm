8d3f8420a1750b75524b551058fb4137ef5a8572
chobolevel <rodaka123@naver.com>
2023-08-29 01:18:28 +0900

    문제) 소문자로 바꾸기

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,20 @@
-import java.util.stream.IntStream;
-
 public class Solution {
 
-  public static int[] solution(int n, int k) {
-    return IntStream.rangeClosed(1, n).filter(i -> i % k == 0).toArray();
+  public static String solution(String myString) {
+    String result = "";
+    for(String s : myString.split("")) {
+      if(s.equals(s.toLowerCase())) {
+        result += s;
+      } else {
+        result += s.toLowerCase();
+      }
+    }
+    return result;
   }
 
   public static void main(String[] args) {
     int[] arr = {5, 7, 8, 3};
+    System.out.println(solution("aBcDeFg"));
   }
 
 }
