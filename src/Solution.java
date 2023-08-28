d28a306bf7b305cc8b8fb2e2d50a074cdc149e71
chobolevel <rodaka123@naver.com>
2023-08-29 01:12:37 +0900

    문제) 이어 붙인 수

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,27 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Solution {
 
   public static int solution(int[] num_list) {
-    int idx = -1;
-    for(int i = 0; i < num_list.length; i++) {
-      if(num_list[i] < 0) {
-        idx = i;
-        break;
-      }
-    }
-    return idx;
-    // return IntStream.range(0, num_list.length).filter(i -> num_list[i] < 0).findFirst().orElseThrow(-1);
+//    String even = "";
+//    String odd = "";
+//    for(int i : num_list) {
+//      if(i % 2 == 0) {
+//        even += String.valueOf(i);
+//      } else {
+//        odd += String.valueOf(i);
+//      }
+//    }
+//    return Integer.parseInt(even) + Integer.parseInt(odd);
+
+    String even = IntStream.of(num_list).filter((i) -> i % 2 == 0).mapToObj(String::valueOf).collect(Collectors.joining());
+    String odd = IntStream.of(num_list).filter((i) -> i % 2 != 0).mapToObj(String::valueOf).collect(Collectors.joining());
+    return Integer.parseInt(even) + Integer.parseInt(odd);
   }
 
   public static void main(String[] args) {
-    int[] arr = {13, 22, 53, 24, 15, 6};
+    int[] arr = {5, 7, 8, 3};
     System.out.println(solution(arr));
   }
 
