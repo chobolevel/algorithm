a7855336683c41feb4f95a90f5f8a9e8f98c972e
chobolevel <rodaka123@naver.com>
2023-08-29 01:14:46 +0900

    문제) 배열 만들기1

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,13 @@
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Solution {
 
-  public static int solution(int[] num_list) {
-//    String even = "";
-//    String odd = "";
-//    for(int i : num_list) {
-//      if(i % 2 == 0) {
-//        even += String.valueOf(i);
-//      } else {
-//        odd += String.valueOf(i);
-//      }
-//    }
-//    return Integer.parseInt(even) + Integer.parseInt(odd);
-
-    String even = IntStream.of(num_list).filter((i) -> i % 2 == 0).mapToObj(String::valueOf).collect(Collectors.joining());
-    String odd = IntStream.of(num_list).filter((i) -> i % 2 != 0).mapToObj(String::valueOf).collect(Collectors.joining());
-    return Integer.parseInt(even) + Integer.parseInt(odd);
+  public static int[] solution(int n, int k) {
+    return IntStream.rangeClosed(1, n).filter(i -> i % k == 0).toArray();
   }
 
   public static void main(String[] args) {
     int[] arr = {5, 7, 8, 3};
-    System.out.println(solution(arr));
   }
 
 }
