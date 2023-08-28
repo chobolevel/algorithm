092e75d491ae2c5a084711a8936f57e1c804846a
chobolevel <rodaka123@naver.com>
2023-08-28 23:22:21 +0900

    문제) 수 조작하기1

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,20 @@
-import java.util.stream.IntStream;
-
 public class Solution {
 
-  public static int[] solution(int start, int end_num) {
-    return IntStream.rangeClosed(-start, -end_num).map(i -> -i).toArray();
+  public static int solution(int n, String control) {
+    String[] controlArr = control.split("");
+    for(String s : controlArr) {
+      switch (s) {
+        case "w" -> n++;
+        case "s" -> n--;
+        case "d" -> n += 10;
+        case "a" -> n -= 10;
+      }
+    }
+    return n;
   }
 
   public static void main(String[] args) {
-    int[] solution = solution(10, 3);
-    for(int i : solution) {
-      System.out.println(i);
-    }
+    System.out.println(solution(0, "wsdawsdassw"));
   }
 
 }
