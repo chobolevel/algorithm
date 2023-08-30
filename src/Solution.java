3f91b13e4b6db1189849cd773434c9234082c819
chobolevel <rodaka123@naver.com>
2023-08-31 00:07:19 +0900

    문제) 잘라서 배열로 저장하기

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
 
-  public static int[] solution(int n) {
-    List<Integer> result = new ArrayList<>();
-    for(int i = 2; i <= n; i++) {
-      while(n % i == 0) {
-        result.add(i);
-        n /= i;
-      }
+  public static String[] solution(String my_str, int n) {
+    int cnt = my_str.length() % n == 0 ? my_str.length() / n : my_str.length() / n + 1;
+    String[] result = new String[cnt];
+    for(int i = 0; i < cnt; i++) {
+      int begin = i * n;
+      int end = ((begin + n) > my_str.length()) ? my_str.length() : (begin + n);
+      result[i] = my_str.substring(begin, end);
     }
-    return result.stream().mapToInt(i -> i).distinct().toArray();
+    return result;
   }
 
   public static void main(String[] args) {
     int[] arr = {5, 7, 8, 3};
-    int[] solution = solution(12);
-    for(int i : solution) {
-      System.out.println(i);
+    String[] result = solution("abc1Addfggg4556b", 6);
+    for(String s : result) {
+      System.out.println(s);
     }
   }
 
